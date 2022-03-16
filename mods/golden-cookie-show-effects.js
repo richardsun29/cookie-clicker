8c91f058b943aa0245a82f9e147c5bf00bae84e9
Richard Sun <richards@wepay.com>
2022-03-16 00:50:19 -0700

    remove demo code

--- mods/golden-cookie-show-effects.js
+++ mods/golden-cookie-show-effects.js
@@ -284,26 +284,10 @@ Game.registerMod('richardsun29/cookie-clicker/mods/golden-cookie-show-effect',{
             else PlaySound('snd/shimmerClick.mp3');
             me.die();
         };
-
-
-
-        Game.registerHook('reincarnate',function(){Game.mods['test mod'].addIntro();});
-        Game.registerHook('check',function(){if (!Game.playerIntro){Game.mods['test mod'].addIntro();}});
-        Game.registerHook('click',function(){Game.Notify(choose(['A good click.','A solid click.','A mediocre click.','An excellent click!']),'',0,0.5);});
-        Game.registerHook('cps',function(cps){return cps*2;});
     },
     save:function(){
-        //note: we use stringified JSON for ease and clarity but you could store any type of string
-        return JSON.stringify({text:Game.playerIntro})
+        return '';
     },
     load:function(str){
-        var data=JSON.parse(str);
-        if (data.text) Game.mods['test mod'].addIntro(data.text);
-    },
-    addIntro:function(text){
-        //note: this is not a mod hook, just a function that's part of the mod
-        Game.playerIntro=text||choose(['oh snap, it\'s','watch out, it\'s','oh no! here comes','hide your cookies, for here comes','behold! it\'s']);
-        if (!l('bakerySubtitle')) l('bakeryName').insertAdjacentHTML('afterend','<div id="bakerySubtitle" class="title" style="text-align:center;position:absolute;left:0px;right:0px;bottom:32px;font-size:12px;pointer-events:none;text-shadow:0px 1px 1px #000,0px 0px 4px #f00;opacity:0.8;"></div>');
-        l('bakerySubtitle').textContent='~'+Game.playerIntro+'~';
     },
 });
