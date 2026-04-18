92800da9c33299e50383960b08d4b2dd5cc4bf35
Richard Sun <richardsun29@gmail.com>
2026-04-17 22:53:56 -0600

    Increase duration values for buffs in effects

--- mods/golden-cookie-show-effects.js
+++ mods/golden-cookie-show-effects.js
@@ -249,11 +249,11 @@ Game.registerMod('richardsun29/cookie-clicker/mods/golden-cookie-show-effect',{
             }
             else if (choice=='click frenzy')
             {
-                buff=Game.gainBuff('click frenzy',Math.ceil(13*effectDurMod),7777777);
+                buff=Game.gainBuff('click frenzy',Math.ceil(13*effectDurMod),77777777);
             }
             else if (choice=='dragonflight')
             {
-                buff=Game.gainBuff('dragonflight',Math.ceil(10*effectDurMod),11111111);
+                buff=Game.gainBuff('dragonflight',Math.ceil(10*effectDurMod),111111111);
                 //if (Math.random()<0.8) Game.killBuff('Click frenzy');
             }
             else if (choice=='chain cookie')
